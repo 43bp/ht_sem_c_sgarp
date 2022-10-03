3ef5618d2ba3b7146f2adf671071f4139a841ac9
43bp2 <43bp@mail.ru>
2022-10-03 20:43:43 +0300

    add HT 0.1-0.4

---
+++ HT_SemC#0.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно без остатка на 2)
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {num} - нечетное");
+}
---
+++ HT_SemC#0.4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите натуральное число (N): ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int end = num - 1;
+if (num < 2)
+{
+    Console.WriteLine($"Четные натуральные числа в диапазоне от {num} до 1 отсутствуют");
+}
+else
+{
+    while (i < end)
+    {
+        i += 2;
+        Console.Write(i);
+        if (i < end)
+        {
+            Console.Write(", ");
+        }
+    }
+}
